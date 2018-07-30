59f4550e2b2bfd27b31779b23847b46d972ceda9
kainanpr <kainanpr95@gmail.com>
2018-07-30 14:23:28 -0300

    Inicio do projeto

---
+++ todo-app/frontend/src/main/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Iniciando projeto</h1>
+        );
+    }
+}
+
+export default App;
