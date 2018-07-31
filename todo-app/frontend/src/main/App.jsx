06a9db11c0d3d0c44fd76b1d8a48af6faf4a6732
kainanpr <kainanpr95@gmail.com>
2018-07-31 09:11:32 -0300

    Criando componente Router

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -2,15 +2,13 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
-import Todo from '../todo/Todo';
-import About from '../about/About';
 import Menu from '../template/Menu';
+import Routes from './Routes';
 
 //Função Arrow
 export default props => (
     <div className="container">
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )
--- todo-app/frontend/src/template/Menu.jsx
+++ todo-app/frontend/src/template/Menu.jsx
@@ -6,15 +6,15 @@ class About extends React.Component {
             <nav className='navbar navbar-inverse bg-inverse'>
             <div className='container'>
                 <div className='navbar-header'>
-                    <a className='navbar-brand' href='#'>
+                    <a className='navbar-brand' href='/'>
                         <i className='fa fa-calendar-check-o'></i> TodoApp
                     </a>
                 </div>
     
                 <div id='navbar' className='navbar-collapse collapse'>
                     <ul className="nav navbar-nav">
-                        <li><a href='#/todos'>Tarefas</a></li>
-                        <li><a href='#/about'>Sobre</a></li>
+                        <li><a href='/todos'>Tarefas</a></li>
+                        <li><a href='/about'>Sobre</a></li>
                     </ul>
                 </div>
             </div>
