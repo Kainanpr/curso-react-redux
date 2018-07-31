60b4333acb2d225a38c4f5912a6be8a5316e264e
kainanpr <kainanpr95@gmail.com>
2018-07-31 09:39:52 -0300

    Alterando componente TodoForm

--- todo-app/frontend/src/todo/TodoForm.jsx
+++ todo-app/frontend/src/todo/TodoForm.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 export default class TodoForm extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Form</h1>
+            <div role="form" className="todoForm">
+                <div className="col-xs-12 col-sm-9 col-md-10">
+                    <input id="description" className="form-control"
+                        placeholder="Adicione uma tarefa" />
+                </div>
+
+                <div className="col-xs-12 col-sm-3 col-md-2">
+                    <button className="btn btn-primary">
+                        <i className="fa fa-plus"></i>
+                    </button>
+                </div>
             </div>
         );
     }
