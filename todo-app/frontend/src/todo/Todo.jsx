8ae29468836886a66f72338afce17a9bb2a50269
kainanpr <kainanpr95@gmail.com>
2018-07-31 11:56:25 -0300

    Alterando componente Todo para realizar requisicao via POST

--- todo-app/frontend/src/todo/Todo.jsx
+++ todo-app/frontend/src/todo/Todo.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/PageHeader';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+
+const URL = "http://localhost:3003/api/todos";
 
 export default class Todo extends React.Component {
     constructor(props) {
@@ -23,7 +27,9 @@ export default class Todo extends React.Component {
     }
 
     handleAdd() {
-        console.log(this.state.description);
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('Funcionou!'));
     }
 
     render() {
