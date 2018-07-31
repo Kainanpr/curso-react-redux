e7d9ea7b6431bcb26e493895f731481aa4b7724f
kainanpr <kainanpr95@gmail.com>
2018-07-31 09:20:49 -0300

    Criando componente PageHeader

--- todo-app/frontend/src/about/About.jsx
+++ todo-app/frontend/src/about/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'; 
+import PageHeader from '../template/PageHeader';
 
 class About extends React.Component {
     render() {
         return (
             <div>
-                <h1>Sobre</h1>
+                <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+                <h2>Nossa História</h2>
+                <p>Lorem ipsum dolor sit amet...</p>
+                <h2>Missão e Visão</h2>
+                <p>Lorem ipsum dolor sit amet...</p>
+                <h2>Imprensa</h2>
+                <p>Lorem ipsum dolor sit amet...</p>
             </div>
         );
     }
