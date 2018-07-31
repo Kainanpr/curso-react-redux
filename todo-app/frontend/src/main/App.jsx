c2bb949a9bff3f6463206d7880a9c16ef75f89f6
kainanpr <kainanpr95@gmail.com>
2018-07-31 08:23:47 -0300

    Criando componente Menu

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -4,10 +4,12 @@ import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import Todo from '../todo/Todo';
 import About from '../about/About';
+import Menu from '../template/Menu';
 
 //Função Arrow
 export default props => (
     <div className="container">
+        <Menu />
         <Todo />
         <About />
     </div>
