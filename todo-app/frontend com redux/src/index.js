ff0469213b4df83b9c03efc35089e1dd493bc4a7
kainanpr <kainanpr95@gmail.com>
2018-08-02 14:26:33 -0300

    Configurando o Redux DevTools (Extensao do Google Chrome)

--- todo-app/frontend com redux/src/index.js
+++ todo-app/frontend com redux/src/index.js
@@ -7,7 +7,13 @@ import { Provider } from 'react-redux';
 import App from './main/App';
 import reducers from './main/reducers';
 
-const store = createStore(reducers);
+/* Integrando o WebApp com o plugin Redux DevTools do Google Chrome */
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+/* Alem de passar os reducers para a store tem que passar a  
+ * referencia para o devtools criado anteriormente */
+const store = createStore(reducers, devTools);
 
 ReactDOM.render(
     <Provider store={store}>
