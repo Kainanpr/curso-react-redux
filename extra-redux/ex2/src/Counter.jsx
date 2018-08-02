66aebc3e8bda516eb8202a26ab1b0c71e40fcf61
kainanpr <kainanpr95@gmail.com>
2018-08-02 11:41:09 -0300

    Concluindo o segundo exemplo

--- extra-redux/ex2/src/Counter.jsx
+++ extra-redux/ex2/src/Counter.jsx
@@ -1,10 +1,32 @@
 //Terceiro arquivo criado
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { inc, dec, stepChanged } from './counterActions';
 
-export default class Counter extends React.Component {
+class Counter extends React.Component {
     render() {
         return (
-            <div>Ok</div>
+            <div>
+                <h1>{this.props.counter.number}</h1>
+                <input onChange={this.props.stepChanged} 
+                    value={this.props.counter.step} 
+                    type="number" />
+                <button onClick={this.props.dec}>Dec</button>
+                <button onClick={this.props.inc}>Inc</button>
+            </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        counter: state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ inc, dec, stepChanged }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
